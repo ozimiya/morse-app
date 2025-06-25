6d17db9f5d706726cdd836e5237655c3ade9934f
ozimiya <ozimiya2@gmail.com>
2025-06-25 11:38:24 +0900

    スタイル調整

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -1,8 +1,8 @@
 export const sanitize = (str: string): string => {
     return str
       .normalize('NFD')
-      .replace(/\u309B/g, '\u3099') // ← これ！通常の濁点→合成用濁点
-      .replace(/\u309C/g, '\u309A') // （おまけ）半濁点も同様に変換
+      .replace(/\u309B/g, '\u3099') // 通常の濁点→合成用濁点
+      .replace(/\u309C/g, '\u309A') // 半濁点も同様に変換
       .replace(/\s/g, '')
       .replace(/[\u3000]/g, '')
       .toUpperCase();
