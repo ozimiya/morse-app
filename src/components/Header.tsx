aae414e66ac0737e4ffa79321223fc9a39860b5f
ozimiya <ozimiya2@gmail.com>
2025-06-26 11:59:40 +0900

    現在画面がactiveになるようheader修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className="app-header">
       <nav>
-        <ul>
-          <li><Link to="/">一文字</Link></li>
-          <li><Link to="/words">単語</Link></li>
-          <li><Link to="/settings">設定</Link></li>
+        <ul className="nav-list">
+          <li>
+            <NavLink to="/" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              一文字
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/words" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              単語
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/settings" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              設定
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
