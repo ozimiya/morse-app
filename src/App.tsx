69db77d518b250d04e5d1ac9a03999313dc30792
ozimiya <ozimiya2@gmail.com>
2025-06-27 18:24:53 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,6 @@ import Settings from './pages/Settings';
 import Header from './components/Header';
 
 function App() {
-
-  // useEffect(() => {
-  //   const setVH = () => {
-  //     const rawHeight =
-  //       window.visualViewport?.height || window.innerHeight;
-  //     const vh = rawHeight * 0.01;
-  //     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  //   };
-
-  //   const handleFocusOut = () => {
-  //     setTimeout(setVH, 100);
-  //   };
-
-  //   // 初期表示に確実に反映
-  //   requestAnimationFrame(setVH);
-
-  //   window.addEventListener('resize', setVH);
-  //   window.visualViewport?.addEventListener('resize', setVH);
-  //   window.addEventListener('focusout', handleFocusOut);
-
-  //   return () => {
-  //     window.removeEventListener('resize', setVH);
-  //     window.visualViewport?.removeEventListener('resize', setVH);
-  //     window.removeEventListener('focusout', handleFocusOut);
-  //   };
-  // }, []);
-
-
   return (
     <>
       <Header />
