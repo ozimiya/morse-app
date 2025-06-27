8acc4c252714f80598c6fa143505ab83eb56587d
ozimiya <ozimiya2@gmail.com>
2025-06-27 17:46:06 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from 'react';
+// import { useLayoutEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import KanaQuiz from './pages/KanaQuiz';
 import WordQuiz from './pages/WordQuiz';
@@ -6,6 +6,16 @@ import Settings from './pages/Settings';
 import Header from './components/Header';
 
 function App() {
+
+  const setVH = () => {
+    const vh = (window.visualViewport?.height || window.innerHeight) * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  window.addEventListener('resize', setVH);
+  window.visualViewport?.addEventListener('resize', setVH); // ← 追加！！
+  setVH();
+
   // ← 高さの調整処理（dev環境でも安定させる）
   // useLayoutEffect(() => {
   //   const setVH = () => {
