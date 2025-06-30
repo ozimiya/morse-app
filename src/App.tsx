e99d3215152fcfc78a5ed829f70939481cce6526
ozimiya <ozimiya2@gmail.com>
2025-06-30 18:19:30 +0900

    更新あった場合の処理

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import KanaQuiz from './pages/KanaQuiz';
 import WordQuiz from './pages/WordQuiz';
 import Settings from './pages/Settings';
 import Header from './components/Header';
 
 function App() {
+  useEffect(() => {
+    const updateSW = registerSW({
+      onNeedRefresh() {
+        if (window.confirm('新しいバージョンがあります。再読み込みしますか？')) {
+          updateSW(true);
+        }
+      },
+      onOfflineReady() {
+        console.log('オフラインで使用できます！');
+      },
+    });
+  }, []);
+
   return (
     <>
       <Header />
